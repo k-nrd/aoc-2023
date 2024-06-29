e4161c1f78e925e1e5967ec3a098f2652eb9ff29
k-nrd <g.konrad@outlook.com>
2024-06-29 16:14:23 -0300

    welp

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,51 +1,102 @@
 open Base
-(** The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
-    For example:
+let numbers =
+  [
+    ("one", (String.Search_pattern.create "one", "1"));
+    ("two", (String.Search_pattern.create "two", "2"));
+    ("three", (String.Search_pattern.create "three", "3"));
+    ("four", (String.Search_pattern.create "four", "4"));
+    ("five", (String.Search_pattern.create "five", "5"));
+    ("six", (String.Search_pattern.create "six", "6"));
+    ("seven", (String.Search_pattern.create "seven", "7"));
+    ("eight", (String.Search_pattern.create "eight", "8"));
+    ("nine", (String.Search_pattern.create "nine", "9"));
+  ]
 
-    1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet
-    In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+let rec peek_and_match line word current_match line_idx word_idx =
+  if equal_string current_match word then Some word
+  else if word_idx >= String.length word || line_idx >= String.length line then
+    None
+  else
+    match (String.get word word_idx, String.get line line_idx) with
+    | a, b when equal_char a b ->
+        peek_and_match line word
+          (String.append current_match (Char.to_string a))
+          (line_idx + 1) (word_idx + 1)
+    | _ -> None
 
-    Consider your entire calibration document. What is the sum of all of the calibration values? *)
+let rec first_some = function
+  | [] -> None
+  | Some x :: _ -> Some x
+  | None :: xs -> first_some xs
+
+let peek_and_match_first line words current line_idx word_idx =
+  first_some
+    (List.map words ~f:(fun word ->
+         peek_and_match line word current line_idx word_idx))
+
+let seq_from_input = function
+  | `String i -> Sequence.of_list (String.split_lines i)
+  | `In_channel i ->
+      Sequence.unfold ~init:i ~f:(fun chan ->
+          match In_channel.input_line chan with
+          | Some line -> Some (line, chan)
+          | None ->
+              In_channel.close chan;
+              None)
+
+let preprocess raw_line =
+  let preprocess_char line char idx =
+    let next_idx = idx + 1 in
+    match char with
+    | 'o' -> peek_and_match line "one" "o" next_idx 1
+    | 't' -> peek_and_match_first line [ "two"; "three" ] "t" next_idx 1
+    | 'f' -> peek_and_match_first line [ "four"; "five" ] "f" next_idx 1
+    | 's' -> peek_and_match_first line [ "six"; "seven" ] "s" next_idx 1
+    | 'e' -> peek_and_match line "eight" "e" next_idx 1
+    | 'n' -> peek_and_match line "nine" "n" next_idx 1
+    | _ -> None
+  in
+  let rec pp line idx =
+    if idx >= String.length line then line
+    else
+      let char = String.get line idx in
+      match preprocess_char line char idx with
+      | Some word ->
+          let pattern, digit =
+            List.Assoc.find_exn ~equal:equal_string numbers word
+          in
+          pp
+            (String.Search_pattern.replace_first pattern ~in_:line ~with_:digit)
+            (idx + 1)
+      | None -> pp line (idx + 1)
+  in
+  let line = pp raw_line 0 in
+  Stdlib.print_endline line;
+  line
 
 let decode input =
-  let seq =
-    match input with
-    | `String i -> String.to_sequence i
-    | `In_channel i ->
-        Sequence.unfold ~init:i ~f:(fun chan ->
-            match In_channel.input_char chan with
-            | Some char -> Some (char, chan)
-            | None ->
-                In_channel.close chan;
-                None)
-  in
   let value = ref 0 in
-  let create_step () =
+  let step raw_line =
+    let line = preprocess raw_line in
     let first_digit = ref '0' in
     let last_digit = ref ' ' in
-    let step char =
+    let process_char char =
       match char with
-      | '\n' ->
-          let first_str = Char.to_string !first_digit in
-          let num =
-            String.append first_str
-              (if equal_char !last_digit ' ' then first_str
-               else Char.to_string !last_digit)
-            |> Int.of_string
-          in
-          value := !value + num;
-          first_digit := '0';
-          last_digit := ' '
       | c when Char.is_digit c ->
           if equal_char !first_digit '0' then first_digit := c
           else last_digit := c
       | _ -> ()
     in
-    step
+    String.iter ~f:process_char line;
+    let first_str = Char.to_string !first_digit in
+    let num =
+      String.append first_str
+        (if equal_char !last_digit ' ' then first_str
+         else Char.to_string !last_digit)
+      |> Int.of_string
+    in
+    value := !value + num
   in
-  Sequence.iter ~f:(create_step ()) seq;
+  Sequence.iter ~f:step (seq_from_input input);
   !value
--- test/test_day1.ml
+++ test/test_day1.ml
@@ -10,9 +10,22 @@ let test_decode_example () =
     (Day1.decode
        (`String "1abc2\n    pqr3stu8vwx\n    a1b2c3d4e5f\n    treb7uchet\n"))
 
+let test_decode_example_2 () =
+  Alcotest.(check int)
+    "correct result" 281
+    (Day1.decode
+       (`String
+         "two1nine\n\
+          eightwothree\n\
+          abcone2threexyz\n\
+          xtwone3four\n\
+          4nineightseven2\n\
+          zoneight234\n\
+          7pqrstsixteen"))
+
 let test_decode () =
   Alcotest.(check int)
-    "correct result" 54697
+    "correct result" 54877
     (Day1.decode (`In_channel (In_channel.open_text "./day1-input.txt")))
 
 (* Run it *)
@@ -22,7 +35,8 @@ let () =
     [
       ( "decode",
         [
-          test_case "Decode example" `Quick test_decode_example;
+          test_case "Decode example 1" `Quick test_decode_example;
+          test_case "Decode example 2" `Quick test_decode_example_2;
           test_case "Decode input" `Quick test_decode;
         ] );
     ]
