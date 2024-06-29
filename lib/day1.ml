724c36feb8605416a5a067645aca5fb843d28559
k-nrd <g.konrad@outlook.com>
2024-06-29 17:32:06 -0300

    rewrote from scratch, still failing with the exact same number

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,40 +1,5 @@
 open Base
 
-let numbers =
-  [
-    ("one", (String.Search_pattern.create "one", "1"));
-    ("two", (String.Search_pattern.create "two", "2"));
-    ("three", (String.Search_pattern.create "three", "3"));
-    ("four", (String.Search_pattern.create "four", "4"));
-    ("five", (String.Search_pattern.create "five", "5"));
-    ("six", (String.Search_pattern.create "six", "6"));
-    ("seven", (String.Search_pattern.create "seven", "7"));
-    ("eight", (String.Search_pattern.create "eight", "8"));
-    ("nine", (String.Search_pattern.create "nine", "9"));
-  ]
-
-let rec peek_and_match line word current_match line_idx word_idx =
-  if equal_string current_match word then Some word
-  else if word_idx >= String.length word || line_idx >= String.length line then
-    None
-  else
-    match (String.get word word_idx, String.get line line_idx) with
-    | a, b when equal_char a b ->
-        peek_and_match line word
-          (String.append current_match (Char.to_string a))
-          (line_idx + 1) (word_idx + 1)
-    | _ -> None
-
-let rec first_some = function
-  | [] -> None
-  | Some x :: _ -> Some x
-  | None :: xs -> first_some xs
-
-let peek_and_match_first line words current line_idx word_idx =
-  first_some
-    (List.map words ~f:(fun word ->
-         peek_and_match line word current line_idx word_idx))
-
 let seq_from_input = function
   | `String i -> Sequence.of_list (String.split_lines i)
   | `In_channel i ->
@@ -45,58 +10,61 @@ let seq_from_input = function
               In_channel.close chan;
               None)
 
-let preprocess raw_line =
-  let preprocess_char line char idx =
-    let next_idx = idx + 1 in
-    match char with
-    | 'o' -> peek_and_match line "one" "o" next_idx 1
-    | 't' -> peek_and_match_first line [ "two"; "three" ] "t" next_idx 1
-    | 'f' -> peek_and_match_first line [ "four"; "five" ] "f" next_idx 1
-    | 's' -> peek_and_match_first line [ "six"; "seven" ] "s" next_idx 1
-    | 'e' -> peek_and_match line "eight" "e" next_idx 1
-    | 'n' -> peek_and_match line "nine" "n" next_idx 1
-    | _ -> None
+let digit_mappings =
+  [
+    ("one", "1");
+    ("two", "2");
+    ("three", "3");
+    ("four", "4");
+    ("five", "5");
+    ("six", "6");
+    ("seven", "7");
+    ("eight", "8");
+    ("nine", "9");
+  ]
+
+let digit_regex = Re.compile Re.digit
+
+let substitute_digits str =
+  let try_match s pos =
+    let m (prefix, _) = String.is_prefix (String.subo s ~pos) ~prefix in
+    m
   in
-  let rec pp line idx =
-    if idx >= String.length line then line
+  let rec step s pos =
+    if pos >= String.length s then s
     else
-      let char = String.get line idx in
-      match preprocess_char line char idx with
-      | Some word ->
-          let pattern, digit =
-            List.Assoc.find_exn ~equal:equal_string numbers word
-          in
-          pp
-            (String.Search_pattern.replace_first pattern ~in_:line ~with_:digit)
-            (idx + 1)
-      | None -> pp line (idx + 1)
+      match List.find digit_mappings ~f:(try_match s pos) with
+      | Some (word, digit) ->
+          let prefix_len = String.length word in
+          step
+            (String.concat
+               [
+                 String.subo s ~len:pos;
+                 digit;
+                 String.subo s ~pos:(pos + prefix_len);
+               ])
+            (pos + 1)
+      | None -> step s (pos + 1)
   in
-  let line = pp raw_line 0 in
-  Stdlib.print_endline line;
-  line
+  step str 0
 
 let decode input =
-  let value = ref 0 in
-  let step raw_line =
-    let line = preprocess raw_line in
-    let first_digit = ref '0' in
-    let last_digit = ref ' ' in
-    let process_char char =
-      match char with
-      | c when Char.is_digit c ->
-          if equal_char !first_digit '0' then first_digit := c
-          else last_digit := c
-      | _ -> ()
+  let step sum raw_line =
+    let line = substitute_digits raw_line in
+    let value =
+      match Re.all digit_regex line with
+      | [] -> "0"
+      | x :: [] ->
+          let first = Re.Group.get x 0 in
+          String.append first first
+      | x :: xs -> (
+          let first = Re.Group.get x 0 in
+          match List.last xs with
+          | Some group ->
+              let last = Re.Group.get group 0 in
+              String.append first last
+          | None -> String.append first first)
     in
-    String.iter ~f:process_char line;
-    let first_str = Char.to_string !first_digit in
-    let num =
-      String.append first_str
-        (if equal_char !last_digit ' ' then first_str
-         else Char.to_string !last_digit)
-      |> Int.of_string
-    in
-    value := !value + num
+    sum + Int.of_string value
   in
-  Sequence.iter ~f:step (seq_from_input input);
-  !value
+  Sequence.fold ~init:0 ~f:step (seq_from_input input)
--- test/test_day1.ml
+++ test/test_day1.ml
@@ -28,6 +28,49 @@ let test_decode () =
     "correct result" 54877
     (Day1.decode (`In_channel (In_channel.open_text "./day1-input.txt")))
 
+let test_decode_segment_1 () =
+  Alcotest.(check int)
+    "correct result" 842
+    (Day1.decode
+       (`String
+         "zlmlk1\n\
+          vqjvxtc79mvdnktdsxcqc1sevenone\n\
+          vsskdclbtmjmvrseven6\n\
+          8jkfncbeight7seven8\n\
+          six8dbfrsxp\n\
+          2zpcbjdxcjfone68six\n\
+          zqmzgfivethreefdnlhpeight8798\n\
+          fivenineone6\n\
+          6sixzvdsprdqlftwonine\n\
+          lqztrmztwo8dg\n\
+          four6onerv2pfhm\n\
+          plvzrs5\n\
+          5282gdnc918\n\
+          pskjsrchjpxoneonenine96fivefour\n\
+          fivefour2hhtprpjndm4\n\
+          6qbdcfdjsd1lmldklflteight\n"))
+
+let test_decode_segment_2 () =
+  Alcotest.(check int)
+    "correct result" 820
+    (Day1.decode
+       (`String
+         "5fiveeightl8veight1pxfptklnhj\n\
+          npllktfive45nhvqjcjgpxx\n\
+          hbfr9mm\n\
+          onerbfkf4threeone\n\
+          7eightone\n\
+          eight2kmjlsix8one61\n\
+          rvrnrdrninenine3zq6jqsr\n\
+          lztlntnsevenpplkhkftq1\n\
+          tfrrjmcvtbmktnxtxkkrcctmc33four5gfqpcjreight\n\
+          33rgcjxfsfqsvxxbxcnrjfndrrfmrtk\n\
+          3627837xhhb8\n\
+          bmjhkkn4pgf\n\
+          qkrsvjclp23\n\
+          5fourzllbmcgkxsevengkrzkpvcmvgtxlrv6\n\
+          fivetczxxvjrrqfive1sevennvj6one3\n"))
+
 (* Run it *)
 let () =
   let open Alcotest in
@@ -37,6 +80,8 @@ let () =
         [
           test_case "Decode example 1" `Quick test_decode_example;
           test_case "Decode example 2" `Quick test_decode_example_2;
+          test_case "Decode segment 1" `Quick test_decode_segment_1;
+          test_case "Decode segment 2" `Quick test_decode_segment_2;
           test_case "Decode input" `Quick test_decode;
         ] );
     ]
